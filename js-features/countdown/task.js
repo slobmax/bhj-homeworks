d8e3c7464d3645f3814fab628cbb2bec96ef5154
Maxim Slobozhanin <slob.max@gmail.com>
2022-09-10 14:37:19 +0400

    task 1 completed

---
+++ js-features/countdown/task.js
@@ -0,0 +1,14 @@
+let timer; 
+let countSec = document.getElementById('timer').textContent;
+counter(); 
+
+function counter() {   
+  countSec--;  
+	document.getElementById('timer').textContent = countSec;	
+  if (countSec <= 0){    
+    clearTimeout(timer);    
+    alert('Вы победили в конкурсе!');  }
+  else {
+    timer = setTimeout(counter, 1000);
+  }
+}
