9ea30ee87c74bf575bf22d5dfedd1f41ad8286d2
Maxim Slobozhanin <slob.max@gmail.com>
2022-09-20 02:29:58 +0400

    finalized

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -6,10 +6,12 @@ for(i = 0; i < document.querySelectorAll('.menu__link').length; i++) {
 	let currentElement = getElement(i);
 	
 	currentElement.onclick = () => { 	
-		if(currentElement.closest('li').querySelector('ul').className === 'menu menu_sub') {			
+		if(currentElement.closest('li').querySelector('ul') && 
+		currentElement.closest('li').querySelector('ul').className === 'menu menu_sub') {			
 			currentElement.closest('li').querySelector('ul').className = 'menu menu_sub menu_active';
 			return false;
-		} else if(currentElement.closest('li').querySelector('ul').className === 'menu menu_sub menu_active') {
+		} else if(currentElement.closest('li').querySelector('ul') && 
+		currentElement.closest('li').querySelector('ul').className === 'menu menu_sub menu_active') {
 			currentElement.closest('li').querySelector('ul').className = 'menu menu_sub';
 			return false;
 		} 
