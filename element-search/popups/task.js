64f3656148cac481d9f218ad076fb022d3a4436d
Maxim Slobozhanin <slob.max@gmail.com>
2022-09-26 01:22:43 +0400

    finalized

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -6,13 +6,13 @@ function inactiveModal(modalWindow) {
 	document.getElementById(modalWindow).className = 'modal'; 
 }
 
-for(i=0; i<document.querySelectorAll('.modal__close').length; i++) {
+for(let i=0; i<document.querySelectorAll('.modal__close').length; i++) {
 	document.querySelectorAll('.modal__close').item(i).onclick = function() {
 		this.closest('.modal').className = 'modal';
 	}
 }
 
-for(i=0; i<document.querySelectorAll('.btn').length; i++) {
+for(let i=0; i<document.querySelectorAll('.btn').length; i++) {
 	document.querySelectorAll('.btn').item(i).onclick = function() {
 		activeModal('modal_success');		
 		inactiveModal('modal_main');
