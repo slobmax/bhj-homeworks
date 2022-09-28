cf6cfd7126a2521e27a46b998a54266ba28899e2
Maxim Slobozhanin <slob.max@gmail.com>
2022-09-28 12:52:31 +0400

    task 1 completed

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,16 +1,16 @@
 let button = document.querySelector('.dropdown');
 
 button.addEventListener('click', () => {
-    document.querySelector('ul').className === 'dropdown__list' ? 
-    document.querySelector('ul').className = 'dropdown__list dropdown__list_active' : 
-    document.querySelector('ul').className = 'dropdown__list';    
+	document.querySelector('ul').className === 'dropdown__list' ? 
+	document.querySelector('ul').className = 'dropdown__list dropdown__list_active' : 
+	document.querySelector('ul').className = 'dropdown__list';    
 });
 
 for(let i = 0; i < document.querySelectorAll('.dropdown__item').length; i++) {
-    let currentItem = document.querySelectorAll('.dropdown__link').item(i);    
+	let currentItem = document.querySelectorAll('.dropdown__link').item(i);    
 
-    currentItem.closest('.dropdown__item').addEventListener('click', (event) => {               
-        document.querySelector('.dropdown__value').textContent = currentItem.textContent;        
-        event.preventDefault();        
-    });
+	currentItem.closest('.dropdown__item').addEventListener('click', (event) => {               
+		document.querySelector('.dropdown__value').textContent = currentItem.textContent;        
+		event.preventDefault();        
+	});
 }
