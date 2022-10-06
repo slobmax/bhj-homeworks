f61244ce54d5f90bb710b41bca75c989ed399c05
Maxim Slobozhanin <slob.max@gmail.com>
2022-10-06 14:26:17 +0400

    task 2 completed

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,14 +1,13 @@
-function setActive() {
-	let arr = Array.from(document.querySelectorAll('.rotator__case'));
-	let index = arr.findIndex(item => item.className === 'rotator__case rotator__case_active');
+setInterval(() => {
+	let arrOfRotator = Array.from(document.querySelectorAll('.rotator__case'));
+	let index = arrOfRotator.findIndex(item => item.classList.contains('rotator__case_active'));
 
-	if(index >= 0 && index < arr.length - 1) {
-			arr[index].className = 'rotator__case';
-			arr[index + 1].className = 'rotator__case rotator__case_active';  
-	} else if(index === arr.length - 1) {
-			arr[index].className = 'rotator__case';
-			arr[index - arr.length + 1].className = 'rotator__case rotator__case_active';  
-	}      
-}
+	if(index >= 0 && index < arrOfRotator.length - 1) {
+		arrOfRotator[index].classList.remove('rotator__case_active');
+		arrOfRotator[index + 1].classList.add('rotator__case_active');  
+	} else if(index === arrOfRotator.length - 1) {
+		arrOfRotator[index].classList.remove('rotator__case_active');
+		arrOfRotator[index - arrOfRotator.length + 1].classList.add('rotator__case_active'); 
+	}   
 
-setInterval(setActive, 1000);
+}, 1000);
