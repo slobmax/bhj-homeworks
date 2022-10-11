c8dfa8da3c78b217e73f901f316b9fbeefc555da
Maxim Slobozhanin <slob.max@gmail.com>
2022-10-12 02:34:18 +0400

    task 3 completed

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -67,26 +67,23 @@ class Autocomplete {
     this.list.innerHTML = html.join('');
   }
 
-  getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
+  getMatches( text ) { 
 
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
-      }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    let obj = {};
+    let arr = [];
+
+    for(let i = 0; i < this.input.options.length; i++) {
+      
+      if(this.input.options[i].text.includes(text)) {
+        
+        obj = {text: this.input.options[i].text, 
+          value: this.input.options[i].value};
+
+        arr.push(obj);
+      }    
+    }     
+    
+    return arr;      
   }
 }
 
