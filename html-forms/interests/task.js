02701e32adc049d45a4b0f7fa80a4926350b3229
Maxim Slobozhanin <slob.max@gmail.com>
2022-10-12 14:22:47 +0400

    task 2 finalized

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,17 +1,11 @@
-const arrOfInput = document.querySelectorAll('input');
+const arrOfUl = Array.from(document.querySelectorAll('.interests_active'));
 
-arrOfInput.forEach(element => element.addEventListener('change', () => {
-
-		const arrOfUl = Array.from(document.querySelectorAll('.interests_active'));
-
-		arrOfUl.forEach(el => {
-			if(el.closest('li').querySelector('input').checked === true) {
-				el.firstElementChild.querySelector('input').checked = true;
-				el.lastElementChild.querySelector('input').checked = true;
-			} else if(el.closest('li').querySelector('input').checked === false) {
-				el.firstElementChild.querySelector('input').checked = false;
-				el.lastElementChild.querySelector('input').checked = false;
-			}
-		})		
-	})
-);
+arrOfUl.forEach(el => el.closest('li').querySelector('input').addEventListener('change', () => {
+	if(el.closest('li').querySelector('input').checked === true) {				
+		el.firstElementChild.querySelector('input').checked = true;
+		el.lastElementChild.querySelector('input').checked = true;
+	} else if(el.closest('li').querySelector('input').checked === false) {
+		el.firstElementChild.querySelector('input').checked = false;
+		el.lastElementChild.querySelector('input').checked = false;
+	}
+}));
