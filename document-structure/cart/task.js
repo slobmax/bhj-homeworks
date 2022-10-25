f842ec0087a87a36f105fbb33031a3fecbd0dd5a
Slobozhanin Maxim <slob.max@gmail.com>
2022-10-26 02:06:01 +0400

    task 3 completed

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,44 +1,40 @@
-const remove = Array.from(document.querySelectorAll('.product__quantity-control_dec'));
-remove.forEach(el => el.addEventListener('click', () => {    
-    if(el.nextElementSibling.textContent > 1) {
-        el.nextElementSibling.textContent--;
+const minus = Array.from(document.querySelectorAll('.product__quantity-control_dec'));
+minus.forEach(el => el.addEventListener('click', () => {    
+    if(el.nextElementSibling.innerText > 1) {
+        el.nextElementSibling.innerText--;
     }    
 }));
 
-const add = Array.from(document.querySelectorAll('.product__quantity-control_inc'));
-add.forEach(el => el.addEventListener('click', () => {
-    el.previousElementSibling.textContent++;
+const plus = Array.from(document.querySelectorAll('.product__quantity-control_inc'));
+plus.forEach(el => el.addEventListener('click', () => {
+    el.previousElementSibling.innerText++;
 }))
-
 
 const addInTrash = Array.from(document.querySelectorAll('.product__add'));
 
-
 addInTrash.forEach(el => el.addEventListener('click', () => {
-    const dataId = el.closest('.product').getAttribute('data-id');
-    const prodImage = el.closest('.product__controls').previousElementSibling.src;
-    let prodCount = el.closest('.product__quantity').querySelector('.product__quantity-value').innerText;
-    const product = `<div class="cart__product" data-id=${dataId}>
-                        <img class="cart__product-image" src=${prodImage}>
-                        <div class="cart__product-count">${prodCount}</div>
+    const cartId = el.closest('.product').getAttribute('data-id');
+    const cartImage = el.closest('.product__controls').previousElementSibling.src;
+    let cartCount = el.closest('.product__quantity').querySelector('.product__quantity-value').innerText;
+    const cart = `<div class="cart__product" data-id=${cartId}>
+                        <img class="cart__product-image" src=${cartImage}>
+                        <div class="cart__product-count">${cartCount}</div>
                     </div>`;
 
     const trash = document.querySelector('.cart__products');
     
-    const prodInTrash = Array.from(document.querySelectorAll('.cart__product'));
-    trash.insertAdjacentHTML('beforeEnd', product); 
-    console.log(prodInTrash);
-    //prodInTrash.forEach(elem => {elem.innerText === dataId ?
-    //elem.querySelector('.cart__product-count').innerText++ :
-    //trash.insertAdjacentHTML('beforeEnd', product);
+    const cartInTrash = Array.from(document.querySelectorAll('.cart__product'));
+        
+    let searchId;
     
-    
-    //trash.insertAdjacentHTML('beforeEnd', product);   
-    //});
-    
-    //prodInTrash.includes(!dataId) ? trash.insertAdjacentHTML('beforeEnd', product) : prodCount++;
-    
-    //trash.insertAdjacentHTML('beforeEnd', product);  
-    
-}))
+    cartInTrash.forEach(elem => {    
+        elem.getAttribute('data-id') === cartId ? searchId = true && 
+        (elem.querySelector('.cart__product-count').innerText = 
+        Number(elem.querySelector('.cart__product-count').innerText) + Number(cartCount)) : 
+        searchId = false;
+    });
 
+    if(!searchId) {
+        trash.insertAdjacentHTML('beforeEnd', cart) 
+    }    
+}))
