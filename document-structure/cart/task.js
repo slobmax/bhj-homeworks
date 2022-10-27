940c180804149e6a2973c9c2992aac0bdd630bec
Maxim Slobozhanin <slob.max@gmail.com>
2022-10-28 02:06:42 +0400

    finalized

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,40 +1,33 @@
 const minus = Array.from(document.querySelectorAll('.product__quantity-control_dec'));
 minus.forEach(el => el.addEventListener('click', () => {    
-    if(el.nextElementSibling.innerText > 1) {
-        el.nextElementSibling.innerText--;
-    }    
+	if(el.nextElementSibling.innerText > 1) {
+			el.nextElementSibling.innerText--;
+	}    
 }));
 
 const plus = Array.from(document.querySelectorAll('.product__quantity-control_inc'));
 plus.forEach(el => el.addEventListener('click', () => {
-    el.previousElementSibling.innerText++;
+  el.previousElementSibling.innerText++;
 }))
 
 const addInTrash = Array.from(document.querySelectorAll('.product__add'));
 
 addInTrash.forEach(el => el.addEventListener('click', () => {
-    const cartId = el.closest('.product').getAttribute('data-id');
-    const cartImage = el.closest('.product__controls').previousElementSibling.src;
-    let cartCount = el.closest('.product__quantity').querySelector('.product__quantity-value').innerText;
-    const cart = `<div class="cart__product" data-id=${cartId}>
-                        <img class="cart__product-image" src=${cartImage}>
-                        <div class="cart__product-count">${cartCount}</div>
-                    </div>`;
+	const cartId = el.closest('.product').getAttribute('data-id');
+	const cartImage = el.closest('.product__controls').previousElementSibling.src;
+	let cartCount = el.closest('.product__quantity').querySelector('.product__quantity-value').innerText;
+	const cart = `<div class="cart__product" data-id=${cartId}>
+											<img class="cart__product-image" src=${cartImage}>
+											<div class="cart__product-count">${cartCount}</div>
+									</div>`;
 
-    const trash = document.querySelector('.cart__products');
-    
-    const cartInTrash = Array.from(document.querySelectorAll('.cart__product'));
-        
-    let searchId;
-    
-    cartInTrash.forEach(elem => {    
-        elem.getAttribute('data-id') === cartId ? searchId = true && 
-        (elem.querySelector('.cart__product-count').innerText = 
-        Number(elem.querySelector('.cart__product-count').innerText) + Number(cartCount)) : 
-        searchId = false;
-    });
+	const trash = document.querySelector('.cart__products');
+	
+	const cartInTrash = Array.from(document.querySelectorAll('.cart__product'));
+			
+	const findCart = cartInTrash.find((item => item.getAttribute('data-id') === cartId));    
 
-    if(!searchId) {
-        trash.insertAdjacentHTML('beforeEnd', cart) 
-    }    
+	findCart ? findCart.querySelector('.cart__product-count').innerText = 
+	Number(findCart.querySelector('.cart__product-count').innerText) + Number(cartCount) :
+	trash.insertAdjacentHTML('beforeEnd', cart);    
 }))
