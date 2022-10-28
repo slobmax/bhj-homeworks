6a4ddd58575e3b23533d6cb33431c938566341a9
Slobozhanin Maxim <slob.max@gmail.com>
2022-10-29 02:46:19 +0400

    finalized

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -5,13 +5,11 @@ const button = document.getElementById('tasks__add');
 function addDiv() {
 
 	const div = `<div class="task">
-					<div class="task__title">${text.value}</div>
-					<a href="#" class="task__remove">&times;</a>
-				</div>`;
+								<div class="task__title">${text.value}</div>
+								<a href="#" class="task__remove">&times;</a>
+							</div>`;
 
 	list.insertAdjacentHTML('afterBegin', div); 
-
-	text.value = '';
 
 	const remove = list.querySelector('.task__remove');
 
@@ -24,9 +22,7 @@ button.addEventListener('click', (e) => {
     
 	if(text.value.trim().length > 0) {        
 		addDiv();       
-	} else {
-		text.value = '';
-	}	
+	}
 
 	e.preventDefault();
 });
