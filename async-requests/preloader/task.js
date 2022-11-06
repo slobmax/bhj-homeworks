8ec99e42392bde289142cde037546b4081fdd98f
Maxim Slobozhanin <slob.max@gmail.com>
2022-11-06 14:34:08 +0400

    finalized

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -6,18 +6,26 @@ xhr.responseType = 'json';
 xhr.send();
 
 xhr.onreadystatechange = () => {
-	if(xhr.readyState === 4) {    
+	if (xhr.status != 200) {
+		const divError = `<div class="item">
+							<div class="item__code">Ошибка ${xhr.status}: ${xhr.statusText}</div>
+							<div class="item__value">Ошибка ${xhr.status}: ${xhr.statusText}</div>
+							<div class="item__currency">руб.</div>
+						</div>`
+
+		cources.insertAdjacentHTML('beforeEnd', divError);		
+	} else {
 		const valutes = xhr.response.response.Valute;        
 		for (const valute in valutes) {            
 			const div = `<div class="item">
-			   						<div class="item__code">${valutes[valute].CharCode}</div>
-										<div class="item__value">${valutes[valute].Value}</div>
-										<div class="item__currency">руб.</div>
-									</div>`
-			
+							<div class="item__code">${valutes[valute].CharCode}</div>
+							<div class="item__value">${valutes[valute].Value}</div>
+							<div class="item__currency">руб.</div>
+						</div>`
+
 			cources.insertAdjacentHTML('beforeEnd', div);
 		}
 		
 		animations.classList.remove('loader_active');
-	};
+	}	
 };    
