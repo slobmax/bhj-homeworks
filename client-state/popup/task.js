2af9d82ba937f819b735927637f2e03675f34f26
Maxim Slobozhanin <slob.max@gmail.com>
2022-11-10 01:15:41 +0400

    task 2 completed

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,6 +1,5 @@
 const modal = document.getElementById('subscribe-modal');
 const modalClose = modal.querySelector('.modal__close');
-//console.log(document.cookie);
 
 if(getCookie('modal') !== 'modal=closed') {	
     modal.classList.add('modal_active');
@@ -12,12 +11,11 @@ modalClose.onclick = () => {
 };
 
 function setCookie(name, value) {	
-	document.cookie = name + '=' + encodeURIComponent(value);
-	console.log(document.cookie);	
-}
+	document.cookie = name + '=' + encodeURIComponent(value);		
+};
 
 function getCookie(name) {
 	const cookieArr = document.cookie.split('; ');
 	const cookie = cookieArr.find(el => el.startsWith(name + '='));		
     return cookie;	
-}
+};
