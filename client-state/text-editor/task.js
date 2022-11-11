d2f8712b7f26743a65126935b7d5b76421f77e43
Maxim Slobozhanin <slob.max@gmail.com>
2022-11-11 13:54:15 +0400

    finalized

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,9 +1,14 @@
 const editor = document.getElementById('editor');
-editor.value = localStorage.editor;
 editor.oninput = () => localStorage.setItem('editor', editor.value);
+
+window.onload = () => {
+    !localStorage ? 
+    editor.value = '' :
+    editor.value = localStorage.editor;
+};
 
 const button = document.getElementById('clear');
 button.onclick = () => {
     editor.value = '';
     localStorage.editor = '';
-};    
+};
